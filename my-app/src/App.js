a1c6e4428498a626473f4632f269aa8759929b44
Deenie-Wallace <jubilant.deenie@gmail.com>
2018-09-23 14:52:57 -0700

    app.js notes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+cdimport React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js'
@@ -16,3 +16,47 @@ class App extends Component {
   }
 }
 export default App;
+
+{
+  /*
+  <App>
+  state = {
+    locationsArr: [],
+    selectedMarkerIndex: -1,
+    center: {} // add default latlng for app
+    zoom: {}
+  }
+
+  handleMarkerClickEvent = (event, latng, index) => {
+    this.setState  ({
+      selectedMarker.Index = index, 
+      center = latlng // clicked latlng
+    })
+  }
+
+  handleinfoWindowCloseEvent => {
+    // Reset state to default
+    this.setstate ({
+      selectMarkerIndex = -1,
+      center= defaultMapCenter,
+      zoom= defaultZoomValue
+    })
+  }
+
+  //  App.js function
+  <ListPlace
+  onKeyPressed = this.handleMarkerClickEvent 
+  locationArray = {//Foursquare API locations}
+  />
+  <Map onMarkerClick = this.handleMarkerClickEvent />
+  
+  </>
+
+
+  <Map>
+
+      // <CompositeMAPUsingReact onMarkerClick = this.props.onMarkerClick />
+  
+  </Map>
+  */
+} 
